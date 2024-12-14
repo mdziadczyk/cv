ffc6306db0fb9faa575a4ffa69e7e8eff63dac08
Magdalena Dziadczyk <dziadczykmagdalena@gmail.com>
2024-12-14 12:34:24 +0100

    remove comments

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Ustaw serwowanie plików statycznych
+
 app.use(express.static(__dirname));
 
-// Obsługa głównego endpointu
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Uruchom serwer
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
